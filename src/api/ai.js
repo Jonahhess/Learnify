72d27252a0395dc70b67e3a86bf38af096b9fede
jonahhess <jonahhessdev@gmail.com>
2025-09-25 23:34:24 +0300

    added all fields to courseware

--- src/api/ai.js
+++ src/api/ai.js
@@ -1,15 +1,16 @@
 import { jsonFetch } from "./auth.js";
 
 export function generateCourseOutlineFromTitle(title) {
+  console.log(title);
   return jsonFetch(`${import.meta.env.VITE_API_BASE_URL}/ai/outline/`, {
     method: "POST",
     body: title,
   });
 }
 
-export function generateCoursewareFromTitle(title) {
+export function generateCoursewareFromTitle(courseTitle, courseId, title) {
   return jsonFetch(`${import.meta.env.VITE_API_BASE_URL}/ai/courseware/`, {
     method: "POST",
-    body: title,
+    body: JSON.stringify({ courseTitle, courseId, title }),
   });
 }
